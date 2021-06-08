8465274c72ca67ee0599dcf7a33f6fe19296ba18
unknown <smd0.bd@gmail.com>
2021-06-08 16:14:27 +0200

    Init project

---
+++ src/main/java/com/devis/phone/DevisPhoneApp.java
@@ -0,0 +1,13 @@
+package com.devis.phone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevisPhoneApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevisPhoneApp.class, args);
+	}
+
+}
